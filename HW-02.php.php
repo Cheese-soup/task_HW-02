4214ae9dc5c8b73bc0e419ee72ff2165545023da
Cheese-soup <aliya.islamova1@gmail.com>
2023-12-12 15:05:41 +0300

    third commit

--- HW-02.php.php
+++ HW-02.php.php
@@ -49,25 +49,67 @@ $example_persons_array = [
 ];
 
 function getPartsFromFullname($fullname){
-    $expl = explode(" ",$fullname);
-    $sprtd = [
-        'surname' => $expl[0],
-        'name' => $expl[1],
-        'patr' => $expl[2],
+    $exploded = explode(" ",$fullname);
+    $separated = [
+        'surname' => $exploded[0],
+        'name' => $exploded[1],
+        'patronymic' => $exploded[2],
     ];
-    return $sprtd;
+    return $separated;
 };
 
-function getFullnameFromParts($surname, $name, $patronomyc){
-    $fullName = [$surname, $name, $patronomyc];
+function getFullnameFromParts($surname, $name, $patronymic){
+    $fullName = [$surname, $name, $patronymic];
     return implode(' ', $fullName);
 };
 
 function getShortName($fullName){
-    $sprtd = getPartsFromFullname($fullName);
-    $shortName = $sprtd["name"] . ' ' . mb_substr($seporated["surname"],0,1) . ".";
+    $separated = getPartsFromFullname($fullName);
+    $shortName = $separated["name"] . ' ' . mb_substr($separated["surname"],0,1) . ".";
 
     return $shortName;
 };
+
+function getGenderFromName($fullName){
+    $separated = getPartsFromFullname($fullName);
+    $gender = 0;
+
+    if (mb_substr($separated["surname"],-2,2) == "ва"){
+        $gender = -1;
+    } elseif (mb_substr($seporated["surname"],-1,1) == "в"){
+        $gender = 1;
+    } else {
+        $gender = 0;
+    }
+    
+    $genderName = mb_substr($separated["name"],-1,1);
+
+    if ($genderName == "a"){
+        $gender = -1;
+    } elseif ($genderName == "й" || $genderName == "н"){
+        $gender = 1;
+    } else {
+        $gender = 0;
+    }
+
+    if (mb_substr($separated["patronymic"],-3,3) == "вна"){
+        $gender = -1;
+    } elseif (mb_substr($seporated["patronymic"],-2,2) == "ич"){
+        $gender = 1;
+    } else {
+        $gender = 0;
+    }
+
+    if (($gender <=> 0) === 1){
+        return "Male";
+    } elseif (($gender <=> 0) === -1){
+        return "Female";
+    } else {
+        return "Undefined";
+    }
+
+}
+
+
 
 ?>
